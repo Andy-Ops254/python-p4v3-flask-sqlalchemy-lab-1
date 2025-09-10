d6f7404f890f2cb92279fbd3fd719cefdd5b1ac1
Andy-Ops254 <ghostandy254@outlook.com>
2025-09-10 12:15:35 +0300

    complete flask sqlalchemy lab 1

--- server/app.py
+++ server/app.py
@@ -21,7 +21,49 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes(id):
+    query = Earthquake.query.filter_by(id=id).first()
+    if query:
+        body = {
+            "id":query.id,
+            "location": query.location,
+            "magnitude": query.magnitude,
+            "year": query.year
+        }
+        return make_response(body, 200)
+    else:
+        body = {
+            "message": f"Earthquake 9999 not found."
+        }
+        return make_response(body, 404)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def quakes (magnitude):
+    query = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()   
+    quakes = [
+            {
+                "id":quake.id,
+                "location": quake.location,
+                "magnitude":quake.magnitude,
+                "year":quake.year
+            } 
+            for quake in query
+            ]
+    count = len(quakes)
+
+    if query:
+        body = {
+            "count": count,
+            "quakes": quakes
+        }
+        return make_response(body, 200)
+    else:
+        body = {
+            "count":0,
+            "quakes":[]
+        }
+        return make_response(body,200)
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, column, String, Float, Integer
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -7,3 +7,14 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__='earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float())
+    location = db.Column(db.String())
+    year = db.Column(db.Integer())
+
+    def __repr__(self):
+        return f'<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>'
+ 
